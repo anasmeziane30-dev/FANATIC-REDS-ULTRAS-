d5761884a34e962c6b9a692f38cfd2a712e9102c
anasmeziane30-dev <anasmeziane30@gmail.com>
2026-07-25 12:53:25 +0100

    Update comments and change environment variable name

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from flask import Flask
 from threading import Thread
 
-# إعداد خويب الويب الوهمي للحفاظ على عمل البوت
+# إعداد خادم الويب الوهمي للحفاظ على عمل البوت 24/7
 app = Flask('')
 
 @app.route('/')
@@ -18,7 +18,7 @@ def keep_alive():
     t = Thread(target=run)
     t.start()
 
-# إعدادات البوت وصلاحياته
+# إعدادات البوت والصلاحيات (تأكد من تفعيلها من موقع ديسكورد)
 intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
@@ -30,13 +30,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
+# أمر التكرار (لتجربة استجابة البوت)
 @bot.command(name='say')
 async def say(ctx, *, message: str):
     await ctx.message.delete() # لحذف رسالتك وإظهار الرسالة نظيفة
     await ctx.send(message)
 
-# تشغيل البوت
+# تشغيل خادم الويب والبوت بشكل آمن
 keep_alive()
-TOKEN = os.environ.get('MTUzMDUyNjE3NjE4MTg4Mjk4MA.GDm4I-.RHIgdRHej6U0PQhBtsrbz9O7nzMLxKiFjlXg7w')
+TOKEN = os.environ.get('DISCORD_TOKEN')
 bot.run(TOKEN)
-
