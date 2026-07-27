a5dc3ac366c57913d07c8e7ef839566d181aa009
anasmeziane30-dev <anasmeziane30@gmail.com>
2026-07-27 08:57:35 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Install ffmpeg for voice support
-apt-get update && apt-get install -y ffmpeg
-
-# Install python requirements
-pip install -r requirements.txt
